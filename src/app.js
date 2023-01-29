fd6fa179540c29ac248487479722834db2148593
Kukuh182 <kukuhpr21@gmail.com>
2023-01-30 04:24:25 +0700

    remove morgan

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
 const routeRegister = require('./routes/route_register');
 
-app.use(morgan);
 app.use('/', routeRegister);
 
 module.exports = app;
