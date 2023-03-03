33129ee0f968eb7426f9decbf6bf28788c7ac9d4
kukuhpr21 <kukuhpr21@gmail.com>
2023-03-03 13:28:54 +0700

    rename middleware

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authorization = require('../middlewares/authorization.middleware');
+const authorization = require('../middleware/authorization.middleware');
 const productController = require('../controllers/products.controller');
 
 // get all data
